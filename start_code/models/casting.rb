e75f9d125510b13f947772caead48e8b7e60da79
Craig McLeod <mcleod.craig@outlook.com>
2020-05-28 15:13:15 +0100

    Populated def initialize for casting

--- start_code/models/casting.rb
+++ start_code/models/casting.rb
@@ -1,3 +1,15 @@
+require_relative("../db/sql_runner")
+
 class Casting
+
+    attr_reader :movie_id, :star_id, :id
+    attr_accessor :fee
+
+    def initialize( options )
+        @id = options['id'].to_i if options['id']
+        @star_id = options['star_id'].to_i
+        @movie_id = options['movie_id'].to_i
+        @fee = options['fee'].to_i
+    end
 
 end
