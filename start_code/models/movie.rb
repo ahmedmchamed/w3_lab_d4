8a39a63e6c8fff0b1680ddb5aadbca7a21c759d5
Craig McLeod <mcleod.craig@outlook.com>
2020-05-28 15:18:29 +0100

    Populated def initialize for movie

--- start_code/models/movie.rb
+++ start_code/models/movie.rb
@@ -1,3 +1,15 @@
+require_relative('../db/sql_runner')
+
 class Movie
+
+    attr_reader :casting_id, :id
+    attr_accessor :title, :genre
+
+    def initialize(options)
+        @id = options['id'].to_i() if options['id']
+        @title = options['title']
+        @genre = options['genre']
+        @casting_id = options['casting_id'].to_i()
+    end
 
 end
